a6b5e8811d84a606e42a170f9ee4ca71a76bd7d9
GiacomoCorradini <corra981@gmail.com>
2023-01-04 16:55:39 +0100

    update display

--- 03_assignment/display.py
+++ 03_assignment/display.py
@@ -5,7 +5,6 @@ import gepetto.corbaserver
 import subprocess
 import os
 import time
-import webbrowser
 
 # Example of a class Display that connect to Gepetto-viewer and implement a
 # 'place' method to set the position/rotation of a 3D visual object in a scene.
@@ -20,40 +19,33 @@ class Display():
         If the window already exists, it is kept in the current state. Otherwise, the newly-created
         window is set up with a scene named 'world'.
         '''
-      
+        l = subprocess.getstatusoutput("ps aux |grep 'gepetto-gui'|grep -v 'grep'|wc -l")
+        if int(l[1]) == 0:
+            os.system('gepetto-gui &')
+        time.sleep(2)
         
         # Create the client and connect it with the display server.
         try:
-            #self.viewer=gepetto.corbaserver.Client()
-            self.viewer=pin.visualize.MeshcatVisualizer
-            self.viewer.initViewer()
-            webbrowser.open(self.viewer.viewer.url())
-            self.viewer.loadViewerModel()
-            self.viewer.display()
-            time.sleep(3)
+            self.viewer=gepetto.corbaserver.Client()
         except:
             print("Error while starting the viewer client. ")
             print("Check whether Gepetto-viewer is properly started")
 
-        # l = subprocess.getstatusoutput("ps aux |grep 'gepetto-gui'|grep -v 'grep'|wc -l")
-        # if int(l[1]) == 0:
-        #     os.system('gepetto-gui &')
-        #     time.sleep(2)
-        # # Open a window for displaying your model.
-        #     try:
-        #         # If the window already exists, do not do anything.
-        #         windowID = self.viewer.gui.getWindowID (windowName)
-        #         print("Warning: window '"+windowName+"' already created.")
-        #         print("The previously created objects will not be destroyed and do not have to be created again.")
-        #     except:
-        #         # Otherwise, create the empty window.
-        #         windowID = self.viewer.gui.createWindow (windowName)
-        #         # Start a new "scene" in this window, named "world", with just a floor.
-        #         self.viewer.gui.createScene("world")
-        #         self.viewer.gui.addSceneToWindow("world",windowID)
+        # Open a window for displaying your model.
+        try:
+            # If the window already exists, do not do anything.
+            windowID = self.viewer.gui.getWindowID (windowName)
+            print("Warning: window '"+windowName+"' already created.")
+            print("The previously created objects will not be destroyed and do not have to be created again.")
+        except:
+            # Otherwise, create the empty window.
+            windowID = self.viewer.gui.createWindow (windowName)
+            # Start a new "scene" in this window, named "world", with just a floor.
+            self.viewer.gui.createScene("world")
+            self.viewer.gui.addSceneToWindow("world",windowID)
 
         # Finally, refresh the layout to obtain your first rendering.
-        # self.viewer.gui.refresh()
+        self.viewer.gui.refresh()
 
     def place(self,objName,M,refresh=True):
         '''
